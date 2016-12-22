09a7236c97a740bde27f212fa14bee2af6fb49b1
Adam Boerema <Adam Boerema>
2016-12-22 09:35:53 -0500

    Initial commit

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+
+	private static MusicPlayer instance;
+	
+	void Awake () {
+		if(instance != null) {
+			Destroy(gameObject);
+		} else {
+			instance = this;
+			GameObject.DontDestroyOnLoad(gameObject);
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
